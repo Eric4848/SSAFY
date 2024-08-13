5b86611a3265c5bb9e1ea3643bc52a2866301e4d
Eric4848 <hajungsu88@gmail.com>
2024-08-13 22:14:57 +0900

    ssafy240813

---
+++ java/algorithm/swea1288.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class swea1288 {
+	static boolean[] is_counted;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			is_counted = new boolean[10];
+			int ratio = 1;
+			while(true) {
+				int now = ratio * N;
+				while(0 < now) {
+					int rest = now % 10;
+					if(!is_counted[rest])
+						is_counted[rest] = true;
+					now /=10;
+				}
+				if(is_full()) {
+					break;
+				}
+				ratio++;
+			}
+			System.out.println("#" + tc + " " + ratio * N);
+		}
+	}
+	static boolean is_full() {
+		for(int i = 0; i < 10; i++) {
+			if(!is_counted[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
